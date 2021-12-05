6e459de1b323d90e88467537cb9f00770eb03cdb
andy <qqqq5953@gmail.com>
2021-12-05 15:00:44 +0800

    測試檔案上傳

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <div class="test">{{ test }}</div>
+  </div>
+  <router-view />
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      test: 'asc'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  background-color: #fff;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+
+  .test {
+    color: red;
+  }
+}
+</style>
